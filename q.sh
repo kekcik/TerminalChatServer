b08b867da73af33ad8c00be4f49e21cfb53cfe2d
Ivan Trofimov <ivan@trofimov.mobi>
2017-05-25 12:38:14 +0300

    auto:

--- q.sh
+++ q.sh
@@ -2,6 +2,9 @@ mvn package
 git add .
 git commit -m "auto: $1"
 git push http://github.com/kekcik/TerminalChatServer master
-sshpass -p "1a296a9daNt2" ssh -o StrictHostKeyChecking root@93.189.43.66 'bash run.sh' 
+sshpass -p "1a296a9daNt2" ssh -o StrictHostKeyChecking=no root@93.189.43.66 "bash run.sh" 
+
+
+
 
 
